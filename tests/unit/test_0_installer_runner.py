fdea685c2a6c182fc16fdc53bf8f2ac9b0c9bc50
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-12 13:04:01 +0000

    refactor: rename source file

--- tests/unit/test_0_installer_runner.py
+++ tests/unit/test_0_installer_runner.py
@@ -1,11 +1,11 @@
 import os
 
-from modules import dummy
+from modules import personalised_message
 
 
 def describe_dummy_kata():
     def should_print_title(capsys):
         """🧪 expect the dummy kata prints the title"""
-        dummy.print_the_title()
+        personalised_message.print_the_title()
         out, _err = capsys.readouterr()
         assert "😊 Welcome to Dummy Kata" in out
